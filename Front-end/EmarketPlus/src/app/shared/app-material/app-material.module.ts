ab342f9c61258d0b28066b51eb8d472b533d2c67
wender.junior <wender.junior@etaure.com.br>
2023-01-13 12:12:21 -0300

    create Entity and structure

---
+++ Front-end/EmarketPlus/src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatTableModule
+  ],
+})
+export class AppMaterialModule { }
