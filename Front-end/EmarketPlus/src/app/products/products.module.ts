0b83c19f67c89dae64caf2ea18a9d745a1247036
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-01-13 15:02:47 -0300

    Begin of the Products Page

--- Front-end/EmarketPlus/src/app/products/products.module.ts
+++ Front-end/EmarketPlus/src/app/products/products.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { ProductsComponent } from './products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductsComponent
+  ],
   imports: [
     CommonModule,
     SharedModule
